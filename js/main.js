cc57d800e0450a0196fdb47645ac25418f49536c
William Rockwood <wrockwood@tadl.org>
2013-03-22 13:41:10 -0400

    massive css updates, added arrow for list items, fixed main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var main = (function()
         panels: null,
         init: function()
         {
-            this.panels = newwink.plugins.AsyncPanels
+            this.panels = new wink.plugins.AsyncPanels
             (
                 {
                     duration: 500,
@@ -18,6 +18,31 @@ var main = (function()
                 }
             );
             document.body.appendChild(this.panels.getDomNode());
+
+            wink.subscribe('/slidingpanels/events/slidestart', {context: this, method: 'toggleBackButtonDisplay', arguments: 'start'});
+            wink.subscribe('/slidingpanels/events/slideend', {context: this, method: 'toggleBackButtonDisplay', arguments: 'end'});
+            scrollTo(0, 0, 0);
+        },
+        toggleBackButtonDisplay: function(params, status)
+        {
+            switch ( params.id )
+            {
+                case 'page1':
+                    if ( status == 'start' )
+                    {
+                        wink.byId('back').style.display = 'none';
+                    }
+
+                    break;
+
+                default:
+                    if ( status == 'end' )
+                    {
+                        wink.byId('back').style.display = 'block';
+                    }
+
+                    break;
+            };
         }
     }
 
