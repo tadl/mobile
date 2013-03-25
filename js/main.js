5f7e82c5317d440d3cd6f7ab416188b9f6e0423f
William Rockwood <wrockwood@tadl.org>
2013-03-25 11:20:21 -0400

    code cleanup

--- js/main.js
+++ js/main.js
@@ -1,46 +1,33 @@
-var main = (function()
-{
-    var main =
-    {
+var main = (function() {
+    var main = {
         panels: null,
-        init: function()
-        {
-            this.panels = new wink.plugins.AsyncPanels
-            (
-                {
-                    duration: 500,
-                    pages:
-                    [
-                        'page1',
-                        {id: 'page2', url: 'locations.html'},
-                        {id: 'page3', url: 'http://www.tadl.org/mobile/news/feed'}
-                    ]
-                }
-            );
+        init: function() {
+            this.panels = new wink.plugins.AsyncPanels ({
+                duration: 500,
+                pages: [
+                    'page1',
+                    {id: 'locations', url: 'locations.html'},
+                    {id: 'news', url: 'http://www.tadl.org/mobile/news/feed'}
+                ]
+            });
             document.body.appendChild(this.panels.getDomNode());
 
             wink.subscribe('/slidingpanels/events/slidestart', {context: this, method: 'toggleBackButtonDisplay', arguments: 'start'});
             wink.subscribe('/slidingpanels/events/slideend', {context: this, method: 'toggleBackButtonDisplay', arguments: 'end'});
             scrollTo(0, 0, 0);
         },
-        toggleBackButtonDisplay: function(params, status)
-        {
-            switch ( params.id )
-            {
+        toggleBackButtonDisplay: function(params, status) {
+            switch ( params.id ) {
                 case 'page1':
-                    if ( status == 'start' )
-                    {
+                    if ( status == 'start' ) {
                         wink.byId('back').style.display = 'none';
                     }
-
                     break;
 
                 default:
-                    if ( status == 'end' )
-                    {
+                    if ( status == 'end' ) {
                         wink.byId('back').style.display = 'block';
                     }
-
                     break;
             };
         }
