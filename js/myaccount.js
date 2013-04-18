f9b0de9711ebd374def73ac0ff2dea72540a2c5d
Jeff Godin <jgodin@tadl.org>
2013-04-18 15:49:02 -0400

    WIP: Add login support via Library REST
    Work in Progress: Add login support via Library REST, currently
    tested against the "dummy" implementation driver. After login, the
    user is fetched via REST (which is currently unauthenticated), and
    the full name displayed in a greeting.
    
    Signed-off-by: Jeff Godin <jgodin@tadl.org>

---
+++ js/myaccount.js
@@ -0,0 +1,89 @@
+var token;
+var user_id;
+
+myaccount_login = function () {
+    var user = wink.byId('user').value;
+    var pass = wink.byId('pass').value;
+
+    var xhr = new wink.Xhr();
+
+    var login_params = [
+        {name: 'user', value: user},
+        {name: 'pass', value: pass}
+    ];
+
+    xhr.sendData(
+        'http://shamrock.lib.tadl.org:3000/api/auth.json', login_params, 'GET', {method: 'onsuccess'}, {method: 'onfailure'}
+    );
+
+    onsuccess = function (request) {
+        var response = request.xhrObject.response;
+        var json = wink.parseJSON(response);
+
+        if (json.user) {
+            // clear the password field
+            wink.byId('pass').value = '';
+            // hide the login form
+            wink.byId('login_div').style.display = 'none';
+            // show the logout button
+            wink.byId('logout_button').style.display = 'block';
+            // store the token and user id in global variables
+            token = json.token;
+            user_id = json.user;
+
+            get_user(json.user);
+        }
+    }
+
+    onfailure = function (request) {
+        alert('xhr failure');
+    }
+
+    return false;
+}
+
+populate_summary = function (user_obj) {
+        wink.byId('myaccount_summary').innerHTML = 'Greetings, <b>' + user_obj.name + '</b>!';
+}
+
+clear_summary = function () {
+    wink.byId('myaccount_summary').innerHTML = '';
+}
+
+get_user = function (user) {
+    var user;
+
+    var xhr = new wink.Xhr();
+
+    xhr.sendData(
+        'http://shamrock.lib.tadl.org:3000/api/v0/user/' + user + '.json', null, 'GET', {method: 'onsuccess'}, {method: 'onfailure'}
+    );
+
+    onsuccess = function (request) {
+        var response = request.xhrObject.response;
+        var json = wink.parseJSON(response);
+
+        if (json.id && json.name) {
+            populate_summary(json);
+        } else {
+            alert('something went wrong in get_user');
+        }
+
+    }
+
+    onfailure = function (request) {
+        alert('xhr failure');
+    }
+
+    return false;
+}
+
+logout = function() {
+    token = undefined;
+    user_id = undefined;
+    clear_summary();
+    wink.byId('login_div').style.display = 'block';
+
+    wink.byId('logout_button').style.display = 'none';
+
+}
