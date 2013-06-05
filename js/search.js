5824ca1fb822f78db8c3b78c69c0a9d243f6f5ba
William Rockwood <wrockwood@tadl.org>
2013-06-05 17:26:12 -0400

    style changes in search.js

--- js/search.js
+++ js/search.js
@@ -10,7 +10,7 @@ do_search = function () {
         {name: 'utf8', value: '%E2%9C%93'},
         {name: 'mt', value: 'ALL'},
         {name: 'sort', value: 'RELEVANCE'},
-        {name: 'avail', value: 'true'},
+        {name: 'avail', value: 'false'},
         {name: 'q', value: terms}
     ]
 
@@ -29,11 +29,23 @@ do_search = function () {
     return false;
 }
 
+hideImage = function(id) {
+    document.getElementById(id).style.display = "none";
+}
+
 populate_results = function (json) {
     wink.byId('search_results').innerHTML = ''; // remove any potential previous results
     var searchAccordion = new wink.ui.layout.Accordion({openMultipleSections: false, autoScroll: true});
+    // if (defined(json[':items'].length)) { // eh? yeah?
     for (var i=0; i<json[':items'].length; i++) {
-        results = searchAccordion.addSection('<div style="margin-right:2em;">' + json[':items'][i][':item'][':title'] + '</div>', '<div>bla bla bla</div>');
+        var item = json[':items'][i][':item'];
+        var title = item[':title'];
+        var author = item[':author'];
+        var summary = item[':summary'];
+        var availability = item[':availability'];
+        var callnumber = item[':callnumber'];
+        var recordid = item[':record_id'];
+        results = searchAccordion.addSection('<div style="margin-right:2em;"><img id="' + recordid + '" onerror="hideImage(' + recordid + ');" style="height:45px;float:left;" src="http://catalog.tadl.org/opac/extras/ac/jacket/small/r/' + recordid + '"><div style="margin-left:50px;">' + author + ' : ' + title + '<br><span style="color:#000;">' + availability + ' : ' + callnumber + '</span></div></div>', '<div>' + summary + '</div>');
     }
     wink.byId('search_results').appendChild(searchAccordion.getDomNode());
 }
